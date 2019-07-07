0df6626ed1f88876cc43d6425c3ca4e864b449d2
Mae Rine <mae.s.rine@gmail.com>
2019-07-07 02:17:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === string.toUpperCase()){
     return "YES INDEED!";
-  } else if (string === string.toLowerCase) {
+  } else if (string === string.toLowerCase()) {
     return "I can\'t hear you";
+  } else {
+    
   }
 }
